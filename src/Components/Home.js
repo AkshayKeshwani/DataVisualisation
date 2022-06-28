77f346703e27735ece9248e4baf12c5fc628be2e
Akshay <akshaykeshwani007@gmail.com>
2022-06-28 11:51:12 +0530

    init

---
+++ src/Components/Home.js
@@ -0,0 +1,56 @@
+import React from "react";
+import * as XLSX from "xlsx"
+import {Button, TextField} from "@mui/material"
+import axios from "axios"
+import ScatterPlot from "./IrisData/ScatterPlot";
+import { useHistory } from "react-router-dom";
+import styledEngine from "@mui/styled-engine";
+import './style.css';
+
+export default function Home(props){
+
+
+    const {file, handleChange} = props
+    const [graph, setGraph] = React.useState("")
+
+    let history = useHistory()
+
+    // console.log(graph == );
+
+    const handleClick = () => {
+      history.push(`/${graph}`)
+    }
+
+    return(
+      <div className="p-5 container" >
+        <div class="row text-center">
+          <div class="row">
+              <h2>Home</h2>
+          </div>
+        </div>
+        <div className="d-flex justify-content-center mt-4">
+          <Button id="btn" className="w-15" variant='contained' component='label'>Choose file
+            <input className="form-control primary" type="file" accept=".csv,.xlsx" id="formFile" hidden onChange={(e) => handleChange(e)}/>
+          </Button>
+          <input 
+            class="form-control w-25"  
+            value={file ? file.name : "No File Chosen"} />
+        </div>
+        <div className="d-flex justify-content-center mt-4">
+          <select
+              class="form-select w-25"
+              value={graph}
+              onChange={(e) => {setGraph(e.target.value)}}
+            >
+              <option value="">Select Graph Type</option>
+              <option value="scatter-plot">Scatter Plot</option>
+
+          </select>
+        </div>
+        <div className="d-flex justify-content-center mt-4">
+          <Button id="btn" className="w-15" variant='contained' component='label' onClick={handleClick}>Submit</Button>
+        </div>
+
+      </div>
+    )
+}
